b84bdd8c3e714546f58ac5ed26e50bb62a8ee99c
Ojochogwu <07afrik@gmail.com>
2023-03-12 11:23:17 +0100

    -sec

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+//api security
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 //connect Database
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
@@ -14,7 +19,19 @@ const bookRouter = require("./routes/books");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windows: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimiter());
 
 //routes
 app.use("/api/v1/auth", authRouter);
